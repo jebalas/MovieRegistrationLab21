ef090d711f18bceec0bff5aebdbc417dfd3b7f4d
Jennifer <jebalas7@gmail.com>
2020-10-12 01:54:01 -0400

    initial commit

---
+++ MovieRegistration/Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+
+namespace MovieRegistration.Controllers
+{
+    public class MovieController : Controller
+    { 
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieResultView(MovieResultViewModel model)
+        {
+            // var movieViewModel = new MovieViewModel();
+
+            return View(model);
+        }
+    }
+}
---
+++ MovieRegistration/Models/MovieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace MovieRegistration.Models
+{
+    public class MovieViewModel
+    {
+        public string ID { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public string Year { get; set; }
+        public string Actors { get; set; }
+        public string Directors { get; set; }
+
+    }
+}
